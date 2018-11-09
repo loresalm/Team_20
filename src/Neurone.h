691f550f8056a205276e712f76b3e8af49fa1269
Julian Raub <julianraub@tsf-444-wpa-7-039.epfl.ch>
2018-11-09 16:26:09 +0100

    encore des ajouts

--- src/Neurone.h
+++ src/Neurone.h
@@ -7,7 +7,7 @@ class Neurone {
 public:
 
 /*!
- * initializes the neuron parameters ???
+ * initializes the neuron by giving 
  */
 	Neurone ();
 /*!
@@ -19,15 +19,21 @@ public:
  */
 	double calcul_relaxation();
 /*!
+ * returns true if the neuron is firing namely when the potential is higher than a certain threshold
+ */
+	bool firing();
+/*!
  * getter for the private attribute potentiel
  */
 	double get_potentiel();
 /*!
- * getter for the private attribute potentiel
+ * getter for the private attribute relaxation
  */
 	double get_relaxation();
 /*!
- * updates the neurone by assining calculating a new potential and a new relaxation 
+ * updates the relaxation (every ms) and the membrane potential (every 0.5 ms)
+ * by claculating the differential equations for each parameter using Euler's method
+ * if the neuron is firing, sets the parameters to specific values depedent of c and d. 
  */
 	void update();
 
@@ -35,9 +41,9 @@ private:
 	double a;
 	double b;
 	double c;
-	double d;
+	double d; 
 	bool state;
 	double potentiel(-65);
-	double relaxation;
+	double relaxation; 
 
 };
