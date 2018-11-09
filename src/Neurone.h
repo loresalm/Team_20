036413f41967d7b8daf91e2632e4463070812038
Julian Raub <julianraub@tsf-444-wpa-6-235.epfl.ch>
2018-11-09 11:49:57 +0100

    ebauche de documentation

--- src/Neurone.h
+++ src/Neurone.h
@@ -1,20 +1,43 @@
+//! Neuron super-class 
+/*!
+ *  class responsible for the creation of a neural cell 
+ */
 class Neurone {
 
 public:
-  Neurone ();
-  double calcul_potentiel();
-  double calcul_relaxation();
-  double get_potentiel();
-  double get_relaxation();
-  void update();
+
+/*!
+ * initializes the neuron parameters ???
+ */
+	Neurone ();
+/*!
+ * calculates the membrane potential
+ */ 
+	double calcul_potentiel();
+/*!
+ * calculate the relaxation value of the neuron
+ */
+	double calcul_relaxation();
+/*!
+ * getter for the private attribute potentiel
+ */
+	double get_potentiel();
+/*!
+ * getter for the private attribute potentiel
+ */
+	double get_relaxation();
+/*!
+ * updates the neurone by assining calculating a new potential and a new relaxation 
+ */
+	void update();
 
 private:
-  double a;
-  double b;
-  double c;
-  double d;
-  bool state;
-  double potentiel(-65);
-  double relaxation;
+	double a;
+	double b;
+	double c;
+	double d;
+	bool state;
+	double potentiel(-65);
+	double relaxation;
 
 };
